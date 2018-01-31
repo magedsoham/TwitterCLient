bdafbbc464611c5ed9b00e179e27db6788337921
Maged Soham <magedsoham13@gmail.com>
2018-01-31 23:43:11 +0200

    Login Done

--- app/src/main/java/com/example/magedsoham/twitterclient/LoginActivity.java
+++ app/src/main/java/com/example/magedsoham/twitterclient/LoginActivity.java
@@ -1,13 +1,75 @@
 package com.example.magedsoham.twitterclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+    public static SharedPreferences sharedPreferences;
+    public static String CONSUMER_KEY = "LyKdR5rrSPIbv8IXQLhPeAufT";
+    public static String CONSUMER_SECRET = "61YwhxILx22SlOxxRU7WPFzaiVyXLXUZnx1ltC4xcqzYz4nOJo";
+    private TwitterLoginButton loginButton;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(CONSUMER_KEY, CONSUMER_SECRET))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+        loginButton = findViewById(R.id.login_button);
+        sharedPreferences = getSharedPreferences("TwitterPrefs", 0);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+                sharedPreferences.edit()
+                        .putString("USER_TOKEN", token)
+                        .putString("USER_SECRET", secret)
+                        .commit();
+                Toast.makeText(getApplicationContext(), "SuccessFully Logged.....ٍ" + token + "....***..." + secret, Toast.LENGTH_LONG).show();
+                // Do something with result, which provides a TwitterSession for making API calls
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+                Toast.makeText(getApplicationContext(), "Fail Logged.....ٍ", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 }
